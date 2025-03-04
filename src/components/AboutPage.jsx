0f36f5fd91d10a69b928f4e85a1441cbb69e3f43
Samir Isakov <samirisakov64@gmail.com>
2025-03-04 20:31:29 +0500

    fix about2

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -14,7 +14,7 @@ export default function AboutPage() {
                         </div>
                         <div className="ha-content " style={{marginLeft: '25px'}}>
                             <h1 style={{fontSize: '40px', fontWeight: '600' , color: 'white'}}>О нас</h1>
-                            <p style={{fontSize: '20px',  fontWeight: '700' , color: 'white'}}> Экскурсии для школьников, каждая поездка имеет свой смысл. Некоторые экскурсии рассчитаны на общее развитие школьников, помогают углубленно изучить определенную тему, а другие доставят приятное времяпрепровождение и приятные ощущения</p>
+                            <p style={{fontSize: '20px',  fontWeight: '700' , color: 'white'}}> Экскурсии для школьников, каждая поездка имеет свой смысл. </p>
                             <p style={{fontSize: '20px',  fontWeight: '700' , color: 'white'}}>Некоторые экскурсии рассчитаны на общее развитие школьников, помогают углубленно изучить определенную тему, а другие доставят приятное времяпрепровождение и приятные ощущения.</p>
                             <p style={{fontSize: '20px',  fontWeight: '700',  color: 'white'}}>Основная задача нашей компании – организация незабываемых экскурсий для школьников.</p>
                             {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique rem harum hic ducimus veniam molestias sunt recusandae sequi et aspernatur. Temporibus modi animi dolores natus voluptates laborum porro quidem perspiciatis perferendis numquam ea officiis assumenda id, esse possimus! Quae temporibus culpa voluptate iusto soluta a harum ea nam animi nulla.</p> */}
